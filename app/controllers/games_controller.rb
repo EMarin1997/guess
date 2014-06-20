0f05a34fc87a6b110a95836cdbb177b9148b7a91
Marin <Emarin1997@gmail.com>
2014-06-20 14:18:36 -0700

    generates answer from the computer

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,8 +6,20 @@ class GamesController < ApplicationController
 	end
 
 	def create
+		@game = Game.new(game_params)
+		@game.answer = rand(10)
+
+		@game.save
+		redirect_to @game
 	end
 
 	def show
+  		@game = Game.find(params[:id])
 	end
+
+	private
+		def game_params
+			params.require(:game).permit(:guess, :answer)
+
+		end
 end
